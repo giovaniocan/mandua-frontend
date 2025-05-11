c677a8e69d74226b23734c258240c7c7d024e880
Gustavo <gsavogustavo@gmail.com>
2025-05-11 10:36:32 -0300

    feat: implementação da tela de seleção de dificuldade

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,9 +1,41 @@
-export function Game() {
-    return (
-      <div className="p-6">
-        <h1 className="text-xl">Tela do Jogo</h1>
-      </div>
-    )
-}
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Card } from '../components/Card';
+import './Dificuldade.css';
 
-  
+export const Dificuldade = () => {
+  const [dificuldadeSelecionada, setDificuldadeSelecionada] = useState('');
+  const navigate = useNavigate();
+
+  const handleClick = (nivel: string) => {
+    setDificuldadeSelecionada(nivel);
+    navigate(`/jogo/${nivel}`);
+  };
+
+  return (
+    <div className="dificuldade-container">
+      <button className="btn-voltar" onClick={() => navigate('/')}>Voltar</button>
+      <Card
+        title="Mandu'a"
+        subtitle=""
+        buttons={[
+          {
+            text: 'Fácil',
+            color: 'green',
+            onClick: () => handleClick('facil')
+          },
+          {
+            text: 'Médio',
+            color: 'blue',
+            onClick: () => handleClick('medio')
+          },
+          {
+            text: 'Difícil',
+            color: 'brown',
+            onClick: () => handleClick('dificil')
+          }
+        ]}
+      />
+    </div>
+  );
+};
