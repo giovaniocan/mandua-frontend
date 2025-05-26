f0a5e1d3746551db5d1cacdad4242baa253ea307
jlealmarins <jlealmarins@gmail.com>
2025-05-26 19:58:03 -0300

    feat: Ajuste das telas de jogo e dificuldade

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,20 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from '../pages/Home'
-import { Game } from '../pages/Game'
+import { Dificuldade } from '../pages/Game'
 import { Ranking } from '../pages/Ranking'
+import Instrucoes from '../pages/Instrucoes'
+import { Idioma } from '../pages/Idioma'
+
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/home" element={<Home />} />
-      <Route path="/jogo" element={<Game />} />
+      <Route path="/jogo" element={<Dificuldade />} />
       <Route path="/ranking" element={<Ranking />} />
+      <Route path="/instrucoes" element={<Instrucoes />} />
+      <Route path="/idioma" element={<Idioma />} />
     </Routes>
   )
 }
