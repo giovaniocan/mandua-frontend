191aded23b55ec37c463719fc0bae8e139bafe3f
Gabriel Caldeira <138264512+GaCaldeira@users.noreply.github.com>
2025-05-29 19:34:22 -0300

    feat: adicionando botão de voltar funcional

--- src/pages/Ranking.tsx
+++ src/pages/Ranking.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Ranking.css';
+import { useNavigate } from 'react-router-dom'; // IMPORTANTE
 
 type Player = {
   name: string;
@@ -8,6 +9,8 @@ type Player = {
 };
 
 export const Ranking: React.FC = () => {
+  const navigate = useNavigate(); // HOOK de navegação
+
   const players: Player[] = [
     { name: 'FULANO 1', score: 888888, time: '01:30:04' },
     { name: 'FULANO 2', score: 777777, time: '01:30:04' },
@@ -17,9 +20,13 @@ export const Ranking: React.FC = () => {
     { name: 'FULANO 6', score: 0, time: '01:30:04' },
   ];
 
+  const handleBack = () => {
+    navigate(-1); // Volta uma página no histórico
+  };
+
   return (
     <div className="ranking-wrapper">
-      <button className="back-button">Voltar</button>
+      <button className="back-button" onClick={handleBack}>Voltar</button>
 
       <div className="ranking-container">
         <h1 className="ranking-title">Ranking</h1>
